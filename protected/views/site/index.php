18fe2f974390c1943002a165145d7a91bc1ad0c2
Pavlik Ilya <pavlik.1@mail.ru>
2014-08-11 12:45:45 +0700

    add framework files

---
+++ protected/views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Привет!!!<i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <code><?php echo __FILE__; ?></code></li>
+	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+</ul>
+<?php echo CHtml::link('Link Text',array('site/login')); ?>
