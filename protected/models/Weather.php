bfbad0446ce7b2e6d35a371f02432bb844aa02db
Pavlik Ilya <pavlik.1@mail.ru>
2014-08-14 13:52:03 +0700

    Обновил модель

--- protected/models/Weather.php
+++ protected/models/Weather.php
@@ -6,17 +6,21 @@
  * The followings are the available columns in table 'weather':
  * @property integer $id
  * @property string $date_forecast
+ * @property integer $partofday
  * @property integer $temp
  * @property integer $humidity
  * @property integer $pressure
- * @property integer $wind_speed
+ * @property double $wind_speed
  * @property integer $wind_deg
  * @property integer $precipitation_id
  * @property integer $station_id
+ * @property integer $provider_id
  *
  * The followings are the available model relations:
- * @property Weatherstation $station
  * @property Precipitation $precipitation
+ * @property Weatherstation $station
+ * @property WindDeg $windDeg
+ * @property Provider $provider
  */
 class Weather extends CActiveRecord
 {
@@ -36,11 +40,12 @@ class Weather extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('date_forecast, temp, humidity, pressure, wind_speed, wind_deg, precipitation_id, station_id', 'required'),
-			array('temp, humidity, pressure, wind_speed, wind_deg, precipitation_id, station_id', 'numerical', 'integerOnly'=>true),
+			array('date_forecast, temp, humidity, pressure, wind_speed, wind_deg, precipitation_id, station_id, provider_id', 'required'),
+			array('partofday, temp, humidity, pressure, wind_deg, precipitation_id, station_id, provider_id', 'numerical', 'integerOnly'=>true),
+			array('wind_speed', 'numerical'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, date_forecast, temp, humidity, pressure, wind_speed, wind_deg, precipitation_id, station_id', 'safe', 'on'=>'search'),
+			array('id, date_forecast, partofday, temp, humidity, pressure, wind_speed, wind_deg, precipitation_id, station_id, provider_id', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -52,8 +57,10 @@ class Weather extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'station' => array(self::BELONGS_TO, 'Weatherstation', 'station_id'),
 			'precipitation' => array(self::BELONGS_TO, 'Precipitation', 'precipitation_id'),
+			'station' => array(self::BELONGS_TO, 'Weatherstation', 'station_id'),
+			'windDeg' => array(self::BELONGS_TO, 'WindDeg', 'wind_deg'),
+			'provider' => array(self::BELONGS_TO, 'Provider', 'provider_id'),
 		);
 	}
 
@@ -65,6 +72,7 @@ class Weather extends CActiveRecord
 		return array(
 			'id' => 'ID',
 			'date_forecast' => 'Date Forecast',
+			'partofday' => 'Partofday',
 			'temp' => 'Temp',
 			'humidity' => 'Humidity',
 			'pressure' => 'Pressure',
@@ -72,6 +80,7 @@ class Weather extends CActiveRecord
 			'wind_deg' => 'Wind Deg',
 			'precipitation_id' => 'Precipitation',
 			'station_id' => 'Station',
+			'provider_id' => 'Provider',
 		);
 	}
 
@@ -95,6 +104,7 @@ class Weather extends CActiveRecord
 
 		$criteria->compare('id',$this->id);
 		$criteria->compare('date_forecast',$this->date_forecast,true);
+		$criteria->compare('partofday',$this->partofday);
 		$criteria->compare('temp',$this->temp);
 		$criteria->compare('humidity',$this->humidity);
 		$criteria->compare('pressure',$this->pressure);
@@ -102,6 +112,7 @@ class Weather extends CActiveRecord
 		$criteria->compare('wind_deg',$this->wind_deg);
 		$criteria->compare('precipitation_id',$this->precipitation_id);
 		$criteria->compare('station_id',$this->station_id);
+		$criteria->compare('provider_id',$this->provider_id);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
