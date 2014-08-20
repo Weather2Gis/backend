027344443d7dbd8fe585072c08209b6f7203d905
SerG3z <serg3z.hard>
2014-08-20 13:01:04 +0700

    testing

--- protected/tests/unit/ParserYandexTest.php
+++ protected/tests/unit/ParserYandexTest.php
@@ -11,11 +11,10 @@ class ParserYandexTest extends PHPUnit_Framework_TestCase{
     public function testSaveToDb(){
 
         $ya = Parser_Yandex::parse(29634);
-
+        $cityNameTest = 'Novosibirsk';
         $this->assertEquals(true, isset($ya['name']));
 
 //        $city = City::model()->findByAttributes(array('name_ru' => $ya['name']));
-//        $cityNameTest = 'Novosibirsk';
 //
 //        $this->assertEquals($city, $cityNameTest);
 //        $this->assertEquals(1, 2);
